637876dde80809c956a51d54d9c6d15c2935a6cc
Marek Drwal <37711928+marekd6@users.noreply.github.com>
2025-12-04 23:18:59 +0100

    comparing sums with SAM and finloops SCRUM-58
    compare finloop variants and models with SAM

--- sam_segment_comparison.py
+++ sam_segment_comparison.py
@@ -1,11 +1,15 @@
-"""finloop_segment_comparison.ipynb
+"""SAM_segment_comparison.ipynb
 
-# PV segmentation finloop
+# PV segmentation
 """
 
-"""Comparison for finloop - YOLO-segment vs. YOLOdetect + SAM
+"""Comparison between models (YOLOdetect + SAM)
 
 source (Huggingface): finloop/yolov8s-seg-solar-panels (aka Rzeszów model)
+
+source (Huggingface): https://huggingface.co/spaces/ArielDrabkin/Solar-Panel-Detector
+
+source (Huggingface): andrewgray11/autotrain-solar-panel-object-detection-50559120777
 
 credits: https://blog.roboflow.com/how-to-use-yolov8-with-sam/ (Roboflow)
 
@@ -13,12 +17,13 @@ XI 25
 
 *MD*
 
-note: a dual-use model (detection + instance segmentation)
-
-requires OBB-versioned datasets
-
 ## libs
 """
+from time import time
+import matplotlib.pyplot as plt
+import cv2
+from ultralytics import YOLO, SAM
+import torch
 
 import matplotlib.pyplot as plt
 import cv2
@@ -52,39 +57,6 @@ either:
 """
 
 def sum_pv_segments(pth, model, nazwa="no_info_run", dev=torch.device('cpu'), print_info=False, disp_img=False, display_coef=100):
-    pv_area = 0
-    yolo_results = model(pth, save=print_info, name=nazwa, stream=True, device=dev, verbose=print_info, retina_masks=True)
-    for i, res in enumerate(yolo_results):
-        dsp = disp_img and i % display_coef == 0 # limit
-        ppth = res.path # nicely conveyed
-        img_w, img_h = res.orig_shape # although YOLO reshapes img when necessary, retina_masks=True makes masks match org img
-        if dsp:
-            image = cv2.cvtColor(cv2.imread(ppth), cv2.COLOR_BGR2RGB)
-            image = torch.tensor(image, device=dev)
-        if res is not None and res.masks is not None:
-            binary_mask = torch.where(res.masks.data == True, 1, 0)
-            mask_sum = binary_mask.sum(axis=0).data
-            mask_sum_damped = torch.where(mask_sum >= 1, 1, 0) # actually some pxs covered multiple times - same object
-            if dsp:
-                bcg_white = torch.ones_like(image)*255
-                new_image = bcg_white * (1 - mask_sum_damped[..., torch.newaxis]) + image * mask_sum_damped[..., torch.newaxis]
-                plt.imshow(new_image.reshape((img_w, img_h, 3)).cpu())
-                plt.title(f"Masked PVs in {ppth[ppth.rfind('/'):]}")
-                plt.axis('off')
-                plt.show()
-                # cv2.imwrite('c.png', new_image.reshape((img_w, img_h, 3)).cpu().numpy())
-            pv_area += mask_sum_damped.sum().div(img_w*img_h) # percentage
-            # print('mask sums', mask_sum.sum(), mask_sum_damped.sum())
-            if print_info:
-                print(i, pv_area.item())
-        if dsp:
-            plt.imshow(image.cpu())
-            plt.title(f"base img {ppth[ppth.rfind('/'):]}")
-            plt.axis('off')
-            plt.show()
-    return pv_area
-
-def sum_pv_segments_sam(pth, model, nazwa="no_info_run", dev=torch.device('cpu'), print_info=False, disp_img=False, display_coef=100):
     pv_area = 0
     yolo_results = model(pth, save=print_info, name=nazwa, stream=True, device=dev, verbose=print_info)
     for i, res in enumerate(yolo_results):
@@ -124,28 +96,31 @@ if __name__ == '__main__':
 
     dev = torch.device('cuda:0') if torch.cuda.is_available() else torch.device('cpu')
 
-    """### model"""
+    """## defs
 
-    model_pt = "best.pt"
-
-    model = YOLO(model=model_pt, task="detect", verbose=True)
+    ### variants
+    """
 
     sam = SAM("sam_b.pt")
 
-    with open('finloop_comparison.csv', 'w') as f:
-        f.write('dataset,mode,area,time\n')
+    models = {
+        'finloop': 'best.pt',
+        'ariel': 'final-mosaic-augmentation.pt',
+        'pools': 'solarpanels_pools_yolov8l-p2_1024_v1.pt'
+    }
 
-    model = YOLO('best.pt')
+    """## run"""
+
+    with open('sam_comparison.csv', 'w') as f:
+        f.write('dataset,model,area,time\n')
+
     for data_key, dataset in datasets.items():
-        t = time()
-        area = sum_pv_segments(dataset, model=model, nazwa=data_key, dev=dev, print_info=False)
-        t = time()-t
-        print('done', 'finloop-seg', data_key, 'in', t)
-        with open('finloop_comparison.csv', 'a') as f:
-            f.write(f'{data_key},segment,{area},{t}\n')
-        t = time()
-        area = sum_pv_segments_sam(dataset, model=model, nazwa=data_key, dev=dev, print_info=False)
-        t = time()-t
-        print('done', 'finloop with SAM', data_key, 'in', t)
-        with open('finloop_comparison.csv', 'a') as f:
-            f.write(f'{data_key},SAM,{area},{t}\n')
+        for model_key, model in models.items():
+            model = YOLO(model)
+            t = time()
+            area = sum_pv_segments(dataset, model=model, nazwa=data_key, dev=dev, print_info=False)
+            t = time()-t
+            print('done', model_key, data_key, 'in', t)
+            with open('sam_comparison.csv', 'a') as f:
+                f.write(f'{data_key},{model_key},{area},{t}\n')
+
