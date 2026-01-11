bb542cca3cae6a8ce13c32830f5995fc764122cb
marekd6 <marekdrwal27.9@gmail.com>
2026-01-11 18:54:11 +0100

    grouped res for other ds

--- tuning/merge_feval.py
+++ tuning/merge_feval.py
@@ -6,21 +6,29 @@ MD, I 26
 import os
 import csv
 
-LN = -2 # rzeszow/train, early ft saved differently (ft4 would be incorrect)
+DS = 'pilot'
+SPLT = 'test'
 DIR = 'tuning/results/'
-OUT = 'merged_feval.csv'
+OUT = 'merged_feval'
+FOUT = f'{OUT}_{DS}_{SPLT}'
 
-with open(f'{DIR}{OUT}', 'w', newline='') as w:
+with open(f'{DIR}{FOUT}.csv', 'w', newline='') as w:
     wrt = csv.DictWriter(w, ['setting', 'Box-F1', 'mAP50', 'mAP50-95', 'Mask-F1'])
     wrt.writeheader()
     for fn in os.listdir(DIR):
         print(fn)
-        if fn != OUT:
+        if not fn.startswith(OUT):
             with open(f'{DIR}{fn}', 'r') as f:
                 r = csv.DictReader(f)
-                ln = list(r)[LN]
-                if fn.find('.pt') == -1:
-                    wrt.writerow({'setting': fn, 'Box-F1': ln['Box-F1'], 'mAP50': ln['mAP50'], 'mAP50-95': ln['mAP50-95'], 'Mask-F1': ln['Mask-F1']})
-                else:
-                    wrt.writerow({'setting': fn, 'Box-F1': ln['Box-F1'], 'mAP50': ln['mAP50'], 'mAP50-95': ln['mAP50-95'], 'Mask-F1': 0})
+                found_ds = False
+                for ln in r:
+                    if ln['dataset'] == DS and ln['split'] == SPLT:
+                        found_ds = True
+                        break
+                print(ln)
+                if found_ds:
+                    if fn.find('.pt') == -1:
+                        wrt.writerow({'setting': fn, 'Box-F1': ln['Box-F1'], 'mAP50': ln['mAP50'], 'mAP50-95': ln['mAP50-95'], 'Mask-F1': ln['Mask-F1']})
+                    else:
+                        wrt.writerow({'setting': fn, 'Box-F1': ln['Box-F1'], 'mAP50': ln['mAP50'], 'mAP50-95': ln['mAP50-95'], 'Mask-F1': 0})
 
