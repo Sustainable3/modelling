dae1cba844a2d2ded00011b23b76d3635ac9b5b7
marekd6 <marekdrwal27.9@gmail.com>
2026-01-25 20:47:18 +0100

    panels launched and landed

--- panels/yolo_pv_segment_sum_val.py
+++ panels/yolo_pv_segment_sum_val.py
@@ -1,31 +1,32 @@
 '''
-Docstring for panels.yolo_pv_segment_sum
+Docstring for panels.yolo_pv_segment_sum_val
 
 YOLO segmentation inference with results transformation
 in a GPU-efficient scheme
+provides visual results for a selected subset of images
 
 MD
 
 I 26
 '''
 
-import torch
+from torch import squeeze
 from time import time
 from ultralytics import YOLO
+from os import listdir
 
 
-data = 'divided_images_jpg/'
-data = 'dane_syntetyczne/images/'
+PROJ = 'PV_inf_check'
 data = 'imagery125_640px/'
-out = 'pv_areas_125.csv'
+out = 'pv_areas_125_mick.csv'
 mod = 'ft26_SGD_finloop.pt'
 v = True
 batch_size = 256
 
 
-def calculate_area_share_batch(imgs: str, model: YOLO, out_f: str):
+def calculate_area_share_batch(imgs, model: YOLO, out_f: str):
     t = time()
-    yolo_results = model.predict(imgs, device='cuda:0', verbose=v, batch=batch_size, stream=True)
+    yolo_results = model.predict(imgs, device='cuda:0', verbose=v, batch=batch_size, stream=True, save=True, project=PROJ, show_labels=False)
     print('setting up batch-streamed YOLO inference took', time()-t)
     t = time()
     for res in yolo_results: # 1 per img, so n in total
@@ -33,7 +34,7 @@ def calculate_area_share_batch(imgs: str, model: YOLO, out_f: str):
             pv_area = 0
             img_w, img_h = 640, 640 # res.orig_shape
             ppth = res.path
-            mask_sum = torch.squeeze(res.masks.data).sum() # [1, 640, 640]
+            mask_sum = squeeze(res.masks.data).sum() # [1, 640, 640]
             pv_area = mask_sum.div(img_w*img_h) # percentage
             print(f'{time()-t},{ppth},{pv_area}\n')
             with open(out_f, 'a') as f:
@@ -43,5 +44,6 @@ def calculate_area_share_batch(imgs: str, model: YOLO, out_f: str):
 
 if __name__ == '__main__':
     model = YOLO(mod)
-    calculate_area_share_batch(data, model, out)
+    all_files = [f'{data}/{f}' for f in listdir(data) if f.find('N-34-50-C-c-4-4') != -1]
+    calculate_area_share_batch(all_files, model, out)
     
